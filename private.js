5965caef74e2da27ae9cc3f3c313b72a7d3b1d40
NassimZoubeir <nassimz@outlook.fr>
2023-03-03 11:51:19 +0100

    affichage des donnes en json dans la console

--- private.js
+++ private.js
@@ -1,16 +1,10 @@
-// Appel de l'API et récupération des données
-fetch("https://www.dummyjson.com/products")
-.then(response => response.json())
-.then(data => {
-// Manipulation des données ici
-})
-.catch(error => console.error(error));
 
 
 // Appel de l'API et récupération des données
 fetch("https://www.dummyjson.com/products")
 .then(response => response.json())
 .then(data => {
+    console.log(data);
   const tableBody = document.getElementById("data");
   data.forEach(product => {
     const row = tableBody.insertRow(-1);
@@ -19,7 +13,7 @@ fetch("https://www.dummyjson.com/products")
     const cell3 = row.insertCell(2);
     const cell4 = row.insertCell(3);
     cell1.innerHTML = product.id;
-    cell2.innerHTML = product.title;
+    cell2.innerHTML = product.name;
     cell3.innerHTML = product.description;
     cell4.innerHTML = product.price;
   });
